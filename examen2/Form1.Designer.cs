a73b082872f0a8860972e68f09244c48453727c5
azig0 <ivon32kissera@gmail.com>
2025-04-01 12:39:09 -0600

    first commit

--- examen2/Form1.Designer.cs
+++ examen2/Form1.Designer.cs
@@ -29,19 +29,71 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(128, 128, 255);
             label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.Snow;
-            label1.Location = new Point(454, 39);
+            label1.Location = new Point(463, 39);
             label1.Name = "label1";
             label1.Size = new Size(262, 45);
             label1.TabIndex = 0;
             label1.Text = "INICIAR SECIÓN";
             label1.Click += label1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(-2, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(400, 338);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ButtonFace;
+            label2.Font = new Font("Nirmala Text", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(463, 146);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 21);
+            label2.TabIndex = 2;
+            label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.ButtonFace;
+            label3.Font = new Font("Nirmala Text", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(463, 198);
+            label3.Name = "label3";
+            label3.Size = new Size(89, 21);
+            label3.TabIndex = 3;
+            label3.Text = "Contraseña";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(625, 144);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(625, 196);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 5;
             // 
             // Form1
             // 
@@ -49,9 +101,15 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(790, 338);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Ingresar al sistema";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -59,5 +117,10 @@
         #endregion
 
         private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
--- examen2/Form1.cs
+++ examen2/Form1.cs
@@ -5,11 +5,18 @@ namespace examen2
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.Image = Image.FromFile("C:/Users/ivon3/Downloads/UAEH/PROGRAMACIÓN/examen2/examen2/imágen/login.jpg");
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+                    
         }
     }
 }
